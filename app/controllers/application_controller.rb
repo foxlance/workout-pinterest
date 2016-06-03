447b3c87206c6552fe12e0798ae9014fb9a2d5a7
Lance Vincent Bumanglag <lancevincent@gmail.com>
2016-06-04 03:13:59 +0800

    Extended devise to support name field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   include Pundit
 
   # Prevent CSRF attacks by raising an exception.
@@ -10,6 +12,11 @@ class ApplicationController < ActionController::Base
 
 
   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 
   def user_not_authorized
     redirect_to root_path, alert: "You are not allowed to perform this action"
